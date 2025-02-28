c108fd0146e55394df892560112c07e881fe96fb
sam-ebe <samuel.ebebenge@gmail.com>
2025-02-28 23:58:02 +0100

    feat: add enemy, follow player

---
+++ Assets/Scripts/EnemyController2.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EnemyController2 : MonoBehaviour
+{
+    public float speed = 1.0f;
+    private Rigidbody2D rb;
+    private Transform target;
+    private Vector2 moveDirection;
+    private float positionDelta;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+        target = GameObject.Find("PlayerCharacter").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (target != null)
+        {
+            positionDelta = Mathf.Abs(target.position.x - transform.position.x);
+            if (positionDelta > 0.1)
+            {
+
+                Vector2 direction = (target.position - transform.position).normalized;
+                //float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg * 90f;
+                //rb.rotation = angle;
+
+                moveDirection = direction;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (target != null)
+        {
+                Vector2 direction = (target.position - transform.position).normalized * speed;
+                rb.linearVelocity = new Vector2(moveDirection.x, moveDirection.y) * speed;
+        }       
+    }
+}
